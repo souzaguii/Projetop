941b9f783b274ab66a6e36caa5424d320e333bf3
Geral <Geral@DESKTOP-9T0SLCT>
2019-11-21 18:44:11 -0300

    relatorio

--- src/main/java/dao/RelatorioDAO.java
+++ src/main/java/dao/RelatorioDAO.java
@@ -5,17 +5,25 @@
  */
 package dao;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.Garantia;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRResultSetDataSource;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
 /**
@@ -23,96 +31,96 @@ import net.sf.jasperreports.view.JasperViewer;
  * @author user
  */
 public class RelatorioDAO {
-    
-   public void GeraGarantia(){
-     try{
-       String SQL = "SELECT max(garantia.id) as max_id FROM garantia";
-        PreparedStatement stmt = Conexao.getConexaoMySQL().prepareStatement(SQL);
-        ResultSet rs = stmt.executeQuery();        
-      if (rs.next()) 
-      {
-        int cod = rs.getInt("max_id");
-        RelatorioDAO relatorioGarantia = new RelatorioDAO();
-        relatorioGarantia.RelatorioGar(cod);
-      }
-        
-       } catch (SQLException ex) {
-        JOptionPane.showMessageDialog(null, "Erro no acesso" + ex);  
-    }
-   }
-    /**
-     *
-     * @param cod
-     */
-    public void RelatorioGar(int cod){
-  
-      
-      try {
-          
+
+    public void RelatorioGar(int cod, String valor) {
+
+        try {
+
             String SQL;
-          SQL = "SELECT\n" +
-"     cliente.`id` AS cliente_id,\n" +
-"     cliente.`Nome` AS cliente_Nome,\n" +
-"     cliente.`CPF` AS cliente_CPF,\n" +
-"     cliente.`telefone` AS cliente_telefone,\n" +
-"     cliente.`cidade` AS cliente_cidade,\n" +
-"     cliente.`endereco` AS cliente_endereco,\n" +
-"     cliente.`Email` AS cliente_Email,\n" +
-"     garantia.`descricao` AS garantia_descricao,\n" +
-"     garantia.`saida_concerto` AS garantia_saida_concerto,\n" +
-"     garantia.`garantia` AS garantia_garantia,\n" +
-"     manutencao.`duracao_garantia` AS manutencao_duracao_garantia,\n" +
-"     garantia.`id` AS garantia_id,\n" +
-"     garantia.`valor` AS garantia_valor\n" +
-"FROM\n" +
-"     `cliente` cliente INNER JOIN `garantia` garantia ON cliente.`Nome` = garantia.`Nome`\n" +
-"     INNER JOIN `manutencao` manutencao ON garantia.`descricao` = manutencao.`descricao`"+
-"     WHERE garantia.`id`= '"+cod+"'"  ;
-            
+            SQL = "SELECT\n"
+                    + "     cliente.`id` AS cliente_id,\n"
+                    + "     cliente.`Nome` AS cliente_Nome,\n"
+                    + "     cliente.`CPF` AS cliente_CPF,\n"
+                    + "     cliente.`telefone` AS cliente_telefone,\n"
+                    + "     cliente.`cidade` AS cliente_cidade,\n"
+                    + "     cliente.`endereco` AS cliente_endereco,\n"
+                    + "     cliente.`Email` AS cliente_Email,\n"
+                    + "     garantia.`descricao` AS garantia_descricao,\n"
+                    + "     garantia.`saida_concerto` AS garantia_saida_concerto,\n"
+                    + "     garantia.`garantia` AS garantia_garantia,\n"
+                    + "     manutencao.`duracao_garantia` AS manutencao_duracao_garantia,\n"
+                    + "     garantia.`id` AS garantia_id,\n"
+                    + "     garantia.`valor` AS garantia_valor\n"
+                    + "FROM\n"
+                    + "     `cliente` cliente INNER JOIN `garantia` garantia ON cliente.`Nome` = garantia.`Nome`\n"
+                    + "     INNER JOIN `manutencao` manutencao ON garantia.`descricao` = manutencao.`descricao`"
+                    + "     WHERE garantia.`id`= '" + cod + "'";
+
+            Map parametros = new HashMap();
+            parametros.clear();
+            parametros.put("logo", this.getClass().getResourceAsStream("/LogoRelatório.png"));
+            parametros.put("valor", valor);
+
             PreparedStatement stmt = Conexao.getConexaoMySQL().prepareStatement(SQL);
             ResultSet rs = stmt.executeQuery();
             JRResultSetDataSource relatorioGarantia = new JRResultSetDataSource(rs);
-            JasperPrint JpPrint = JasperFillManager.fillReport("relatorios/garantia.jasper", new HashMap(),relatorioGarantia );
-            JasperViewer jc = new JasperViewer(JpPrint,false);
+            JasperPrint JpPrint = JasperFillManager.fillReport("relatorios/garantia.jasper", parametros, relatorioGarantia);
+            JasperViewer jc = new JasperViewer(JpPrint, false);
             jc.setVisible(true);
             jc.toFront();
-          
-            }
-         catch (SQLException | JRException e) {
+
+        } catch (SQLException | JRException e) {
             System.out.println(e.getMessage());
-             }
-   }
-    
-  
+        }
+    }
 
     public void RelatorioCliente() {
         //To change body of generated methods, choose Tools | Templates.
-     try {
-            String SQL = "SELECT\n" +
-"     cliente.`id` AS cliente_id,\n" +
-"     cliente.`Nome` AS cliente_Nome,\n" +
-"     cliente.`CPF` AS cliente_CPF,\n" +
-"     cliente.`telefone` AS cliente_telefone,\n" +
-"     cliente.`cidade` AS cliente_cidade,\n" +
-"     cliente.`endereco` AS cliente_endereco,\n" +
-"     cliente.`Email` AS cliente_Email\n" +
-"FROM\n" +
-"     `cliente` cliente";
+        try {
+            String SQL = "SELECT\n"
+                    + "     cliente.`id` AS cliente_id,\n"
+                    + "     cliente.`Nome` AS cliente_Nome,\n"
+                    + "     cliente.`CPF` AS cliente_CPF,\n"
+                    + "     cliente.`telefone` AS cliente_telefone,\n"
+                    + "     cliente.`cidade` AS cliente_cidade,\n"
+                    + "     cliente.`endereco` AS cliente_endereco,\n"
+                    + "     cliente.`Email` AS cliente_Email\n"
+                    + "FROM\n"
+                    + "     `cliente` cliente";
+
+            Map parametros = new HashMap();
+            parametros.clear();
+            parametros.put("logo", this.getClass().getResourceAsStream("/LogoRelatório.png"));
+
             PreparedStatement stmt = Conexao.getConexaoMySQL().prepareStatement(SQL);
             ResultSet rs = stmt.executeQuery();
             JRResultSetDataSource relatorioCli = new JRResultSetDataSource(rs);
-            JasperPrint jpPrint = JasperFillManager.fillReport("relatorios/relatorioCliente.jasper", new HashMap(),relatorioCli );
-            JasperViewer jv = new JasperViewer(jpPrint,false);
+            JasperPrint jpPrint = JasperFillManager.fillReport("relatorios/relatorioCliente.jasper", parametros, relatorioCli);
+            JasperViewer jv = new JasperViewer(jpPrint, false);
             jv.setVisible(true);
             jv.toFront();
-          
-            }
-         catch (SQLException | JRException e) {
+
+        } catch (JRException e) {
             System.out.println(e.getMessage());
-             }
-    }  
+        } catch (SQLException ex) {
+            Logger.getLogger(RelatorioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-    
-   }
-   
+    public void GeraGarantia(String valor) {
+        try {
+            String SQL = "SELECT max(garantia.id) as max_id FROM garantia";
+            PreparedStatement stmt = Conexao.getConexaoMySQL().prepareStatement(SQL);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int cod = rs.getInt("max_id");
+                RelatorioDAO relatorioGarantia = new RelatorioDAO();
+                relatorioGarantia.RelatorioGar(cod, valor);
+            }
 
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro no acesso" + ex);
+        }
+    }
+
+}
